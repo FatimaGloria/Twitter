7b10d6e8212978d671ca8e1c375f3b2d20df3904
FatimaGloria <janettgloria1@gmail.com>
2022-05-12 14:12:10 -0500

    6. Se modifica prueba para ajustar al requerimiento 2, se ejecuta y se verifica que falle

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,14 +3,16 @@ const User = require('./../../app/models/User')
 describe ("Unit Tests for User class", () =>{
     test ('Create an User object', ()=>{
     //código que vamos a usar en la app
-    const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+    const user = new User(1, "carlogilmar", "Carlo", "Bio")
     
     //codigo de validación de resultados
     expect(user.id).toBe(1)
     expect(user.userName).toBe("carlogilmar")
     expect(user.name).toBe("Carlo")
     expect(user.bio).toBe("Bio")
-    expect(user.dateCreated).toBe("dateCreated")
-    expect(user.lastUpdated).toBe("lastUpdated")
+    
+    //Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined() // .not.toBeUndefined verifica que tenga un valor
     });
 })
