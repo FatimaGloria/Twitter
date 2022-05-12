b1643dd1a056fadfa4206e352d5e7b30c648cc6d
FatimaGloria <janettgloria1@gmail.com>
2022-05-12 14:33:28 -0500

    Se agrega nueva prueba para el requerimiento 3 : Añadiendo getters

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,8 @@
 const User = require('./../../app/models/User')
 
 describe ("Unit Tests for User class", () =>{
-    test ('Create an User object', ()=>{
+    
+    test ('Create an User object', () => { 
     //código que vamos a usar en la app
     const user = new User(1, "carlogilmar", "Carlo", "Bio")
     
@@ -14,5 +15,21 @@ describe ("Unit Tests for User class", () =>{
     //Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
     expect(user.dateCreated).not.toBeUndefined()
     expect(user.lastUpdated).not.toBeUndefined() // .not.toBeUndefined verifica que tenga un valor
+    });
+    
+    /*
+    Requerimiento 3: Cada objeto tipo user necesita los siguietes gettes: `getUsername`, `getBio`, `getDateCreated`, `getLastUpdated`.
+    Solución: Agrega los getters regresando la propiedad indicada, no olvides usar `this`.
+    */  
+   
+    test ('Add getters', () => {
+        //código que vamos a usar en la app
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        
+        //codigo de validación de resultados
+        expect(user.getUserName).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
     });
 })
